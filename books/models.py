2aaab1d4e5d8f91c7859c7c2d4aef6f8534a8d93
qbarefoot <quintenbarefoot@gmail.com>
2021-06-22 09:57:22 -0400

    Admin set up, ability to add books

--- books/models.py
+++ books/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+
 
 
 class User(AbstractUser):
@@ -8,3 +10,14 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    book_URL = models.URLField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
